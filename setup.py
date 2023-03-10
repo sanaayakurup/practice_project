729b80b2916c03de929a1d220fd61abcdfa49cb6
Sanaaya Rajesh Kurup <sanaayak@cybage.com>
2023-03-10 15:00:22 +0530

    new

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup #will find all the packages 
+
+def get_requirements(file_path):
+    ''' 
+    This func returns a list of required pacakges
+    '''
+    requirements=[]
+    hyphene="-e ."
+    with open(file_path,'r') as file:
+        requirements=file.readlines()
+        requirements=[r.replace("\n","") for r in requirements]
+
+        if hyphene in requirements:
+            requirements.remove(hyphene)
+    return requirements
+
+
+setup(
+name='mlproject'
+,version='0.0.1'
+,author='sanaaya'
+,author_email='sanaaya.kurup@gmail.com'
+,packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
